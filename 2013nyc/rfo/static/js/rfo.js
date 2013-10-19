38399845d3bcd2fef41fb60d24cca1dacdf77603
Brian McFee <brm2132@columbia.edu>
2013-10-19 11:57:16 -0400

    basic functionality works

--- 2013nyc/rfo/static/js/rfo.js
+++ 2013nyc/rfo/static/js/rfo.js
@@ -1,6 +1,5 @@
 
-$("#form").submit(function(event) { get_data(); event.preventDefault();});
-$("#fight").on('click', get_data);
+$("#myform").submit(function(event) { get_data(); event.preventDefault();});
 
 function get_data() {
     var artist1 = $('#artist1').val();
@@ -9,7 +8,8 @@ function get_data() {
     console.log('Artist 1: ' + artist1);
     console.log('Artist 2: ' + artist2);
 
-    /* Disable inputs, set up the spinner */
+    /* switch to the spinner tab*/
+    $("#main").addClass('hide');
 
     /* Execute the query */
 
@@ -18,6 +18,85 @@ function get_data() {
         url: "/query/" + artist1 + "/" + artist2,
         dataType: "json"
     }).done(function(results) {
+
+        /* switch to the fight tab */
+
+        $("#thefight").removeClass('hide');
+
         console.log(results);
+        $("#artist1-name").html(results.playlist[0][0].artist);
+        $("#artist2-name").html(results.playlist[1][0].artist);
+
+        run_the_fight(results.playlist[0], results.playlist[1]);
     });
-});
+}
+
+
+function run_the_fight(pl1, pl2) {
+
+    var total_health_1 = 0.0;
+    var total_health_2 = 0.0;
+
+    var num_songs = pl1.length;
+
+    for (var i = 0; i < num_songs; i++) {
+        total_health_2 += pl1[i].score;
+        total_health_1 += pl2[i].score;
+    }
+
+    var health_1 = total_health_1;
+    var health_2 = total_health_2;
+
+    console.log('Player 1: ' + total_health_1);
+    console.log('Player 2: ' + total_health_2);
+
+    function update_p1() {
+        $("#health1").css('width', (100 * health_1 / total_health_1) + '%');
+    }
+    function update_p2() {
+        $("#health2").css('width', (100 * health_2 / total_health_2) + '%');
+    }
+
+    function fight_round(round_i, player) {
+
+        if (round_i >= num_songs) {
+            return;
+        }
+
+        if (player == 0) {
+            health_2 -= pl1[round_i].score;
+            // add pl1[round_i].track to player 1's list
+            console.log(pl1[round_i]);
+            console.log('Player 2: ' + (health_2 / total_health_2));
+        } else {
+            health_1 -= pl2[round_i].score;
+            // add pl2[round_i].track to player 2's list
+            console.log(pl2[round_i]);
+            console.log('Player 1: ' + (health_1 / total_health_1));
+        }
+        update_p1();
+        if (health_1 <= 1e-10) {
+            // Player 2 wins
+            console.log('Player 2 wins!');
+            return;
+        }
+
+        update_p2();
+        if (health_2 <= 1e-10) {
+            // Player 1 wins
+            console.log('Player 1 wins!');
+            return;
+        }
+
+
+        setTimeout( function() {
+            fight_round(round_i + player, (player + 1) % 2);
+        }, 500);
+    }
+
+    // Start the fight
+    update_p1();
+    update_p2();
+    fight_round(0, 0);
+}
+
