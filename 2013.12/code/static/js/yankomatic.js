4a713af7820dad16d0ac808901cdac2a5ce0d7fc
Brian McFee <brm2132@columbia.edu>
2013-12-07 18:07:19 -0500

    checking in css and javascript

---
+++ 2013.12/code/static/js/yankomatic.js
@@ -0,0 +1,26 @@
+$("#myform").submit(function(event) {
+    get_data();
+    event.preventDefault();
+});
+
+function get_data() {
+    var query = $("#query").val();
+
+    // Execute the query
+    $.ajax({
+        url: "/search/" + query,
+        dataType: "json"
+    }).done(function(results) {
+        // Populate the results list
+        
+        $("#results > li").remove();
+        if (results.length == 0) {
+            // No results, not funny
+            return;
+        }
+
+        for (var i = 0; i < results.length; i++){
+            $("#results").append($('<li class="list-group-item"></li>').html(results[i]));
+        }
+    });
+}
